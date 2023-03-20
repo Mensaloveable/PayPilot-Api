e0bf3c628ab7cfc14cb40ad3ff32e73cfea22349
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-03-20 06:35:19 +0100

    setup

---
+++ src/main/java/com/paypilot/paypilotapi/PayPilotApiApplication.java
@@ -0,0 +1,13 @@
+package com.paypilot.paypilotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPilotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayPilotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paypilot/paypilotapi/PayPilotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypilot.paypilotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPilotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
